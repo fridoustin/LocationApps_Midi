6a3bcb7309a0d90eb37847557b4ebbd07f62c597
fridoustin <fridolinaustin16@gmail.com>
2025-08-27 15:36:21 +0700

    feat: add global color

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color black = Color(0xFF2E2E2E);
+  static const Color primaryColor = Color(0xFFD11A22);
+  static const Color secondaryColor = Color(0xFF305D9F);
+  static const Color cardColor = Color(0xFFFFFFFF);
+  static const Color errorColor = Color(0xFFD11A22);
+  static const Color warningColor = Color(0xFFFFC107);
+  static const Color successColor = Color(0xFF2ECC71);
+  static const Color iconColor = Color(0xFFD9D9D9);
+  
+}
