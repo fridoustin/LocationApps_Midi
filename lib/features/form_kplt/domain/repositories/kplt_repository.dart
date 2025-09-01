b72cbed0094b956d504303c519d167640c2d8af4
fridoustin <fridolinaustin16@gmail.com>
2025-09-01 16:09:36 +0700

    feat(kplt): make form kplt screen

---
+++ lib/features/form_kplt/domain/repositories/kplt_repository.dart
@@ -0,0 +1,6 @@
+import 'package:midi_location/features/form_kplt/domain/entities/form_kplt.dart';
+
+abstract class KpltRepository {
+  Future<List<FormKPLT>> getRecentKplt(String query);
+  Future<List<FormKPLT>> getHistoryKplt(String query);
+}
