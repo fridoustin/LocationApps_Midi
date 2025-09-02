d9c0233bd0bb81b11509e9d978cdf3be85d9e1a1
fridoustin <fridolinaustin16@gmail.com>
2025-09-02 09:45:53 +0700

    chore(main): make debug checker false

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme()
       ),
       home: const AuthGate(),
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: routeGenerators,
     );
   }
