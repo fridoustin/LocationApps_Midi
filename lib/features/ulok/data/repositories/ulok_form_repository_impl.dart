4f4f12f4030d9af3d3c90ef9705208798da8112d
fridoustin <fridolinaustin16@gmail.com>
2025-09-15 14:54:59 +0700

    feat(ulok): update repository to make save and delete draft function

--- lib/features/ulok/data/repositories/ulok_form_repository_impl.dart
+++ lib/features/ulok/data/repositories/ulok_form_repository_impl.dart
@@ -1,16 +1,26 @@
+import 'package:midi_location/features/ulok/data/datasources/ulok_form_local_datasource.dart';
 import 'package:midi_location/features/ulok/data/datasources/ulok_form_remote_datasource.dart';
 import 'package:midi_location/features/ulok/domain/entities/ulok_form.dart';
 import 'package:midi_location/features/ulok/domain/repositories/ulok_form_repository.dart';
 
 class UlokFormRepositoryImpl implements UlokFormRepository {
-  final UlokFormRemoteDataSource _dataSource;
-  UlokFormRepositoryImpl(this._dataSource);
+  final UlokFormRemoteDataSource _remoteDataSource;
+  final UlokFormLocalDataSource _localDataSource; // Tambahkan ini
+
+  UlokFormRepositoryImpl(this._remoteDataSource, this._localDataSource);
 
   @override
-  Future<void> submitUlok(UlokFormData data, String branchId) =>
-      _dataSource.submitUlok(data, branchId);
+  Future<void> submitUlok(UlokFormData data, String branchId) => _remoteDataSource.submitUlok(data, branchId);
       
   @override
-  Future<void> updateUlok(String ulokId, UlokFormData data) =>
-      _dataSource.updateUlok(ulokId, data);
+  Future<void> updateUlok(String ulokId, UlokFormData data) => _remoteDataSource.updateUlok(ulokId, data);
+
+  @override
+  Future<void> saveDraft(UlokFormData data) => _localDataSource.saveDraft(data);
+  
+  @override
+  Future<List<UlokFormData>> getDrafts() => _localDataSource.getDrafts();
+  
+  @override
+  Future<void> deleteDraft(String localId) => _localDataSource.deleteDraft(localId);
 }
