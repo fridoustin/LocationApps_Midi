71fe72ef1ad406916386e4b4250d2b1d010d0dcd
fridoustin <fridolinaustin16@gmail.com>
2025-09-17 10:07:56 +0700

    feat: Implement password reset flow via deep link

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: use_super_parameters, sized_box_for_whitespace
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,6 +9,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:midi_location/auth_gate.dart';
 import 'package:midi_location/core/routes/route.dart';
 import 'package:midi_location/core/services/notification_service.dart';
+import 'package:midi_location/features/auth/presentation/pages/update_password_screen.dart';
 import 'package:midi_location/firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -37,13 +39,44 @@ Future<void> main() async {
 
 final supabase = Supabase.instance.client;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+class _MyAppState extends ConsumerState<MyApp> {
+  late final StreamSubscription<AuthState> _authSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupAuthListener();
+  }
+
+  void _setupAuthListener() {
+    _authSubscription = supabase.auth.onAuthStateChange.listen((data) {
+      final AuthChangeEvent event = data.event;
+      
+      if (event == AuthChangeEvent.passwordRecovery) {
+        navigatorKey.currentState?.pushNamedAndRemoveUntil(
+          UpdatePasswordPage.route,
+          (route) => false,
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _authSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: navigatorKey,
+      navigatorKey: navigatorKey, // Pastikan navigatorKey sudah terpasang
       title: 'Midi Location App',
       theme: ThemeData(
         primarySwatch: Colors.red,
